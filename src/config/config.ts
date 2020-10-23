aeb235b2906edf65cf779116896d830881271290
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-10-23 12:10:13 +0300

    Described the class Config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,13 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+/**
+ * Class for settings (data from .env and etc)
+ */
 class Config {
+    /**
+     * The Port you are using
+     */
     static port = process.env.PORT;
 }
 
