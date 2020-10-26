e5d88b8efde4ababd4cf641ea100d58613300f9d
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-10-26 20:16:16 +0300

    Documented properties

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,19 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 /**
  * Class for settings (data from .env and etc)
  */
 class Config {
-    /**
-     * The Port you are using
-     */
-    public static http_port: number = +process.env.HTTP_PORT!;
-    public static ws_port: number = +process.env.WS_PORT!;
-
-
+  /**
+   * The Port you are using for http connection
+   */
+  public static http_port: number = +process.env.HTTP_PORT!;
+  /**
+   * The Port you are using for web socket connection
+   */
+  public static ws_port: number = +process.env.WS_PORT!;
 }
 
 export default Config;
