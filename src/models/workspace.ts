304243e2c25bb981a154293089f67fd3850884e1
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-10-30 15:52:23 +0300

    linter fixed

--- src/models/workspace.ts
+++ src/models/workspace.ts
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
 
 const workspaceSchema: mongoose.Schema = new mongoose.Schema({
-  name: { type: String, required: true },
-  servers: [{
-    name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  servers: [ {
+    name: {
+      type: String,
+      required: true,
+    },
     token: String,
-    services: [{
+    services: [ {
       type: { type: String },
       payload: mongoose.Schema.Types.Mixed,
-    }],
-  }],
+    } ],
+  } ],
 });
 
 export default mongoose.model('Workspace', workspaceSchema);
