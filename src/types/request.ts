260b8db88d577f3bc1a0c04c0cbafde95505635d
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-10-30 15:21:31 +0300

    Rename file
    from index.ts to request.ts

---
+++ src/types/request.ts
@@ -0,0 +1,15 @@
+/**
+ * Interface for typing requests
+ */
+interface Request {
+  /**
+   * Workspace token?
+   */
+  messageId: string;
+  /**
+   * Type of request
+   */
+  type: string;
+}
+
+export default Request;
