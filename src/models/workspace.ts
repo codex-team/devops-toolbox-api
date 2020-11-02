27cdd37339cff2c8457b508106f2e06252041e77
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-02 13:54:56 +0300

    Described with JSDoc

--- src/models/workspace.ts
+++ src/models/workspace.ts
@@ -1,18 +1,42 @@
 import mongoose from 'mongoose';
 
+/**
+ * Workspace Schema
+ */
 const workspaceSchema: mongoose.Schema = new mongoose.Schema({
+  /**
+   * Workspace name
+   */
   name: {
     type: String,
     required: true,
   },
+  /**
+   * Workspace servers
+   */
   servers: [ {
+    /**
+     * Server name
+     */
     name: {
       type: String,
       required: true,
     },
+    /**
+     * Server token
+     */
     token: String,
+    /**
+     * Server services
+     */
     services: [ {
+      /**
+       * Service type (nginx, docker, ports, intefaces, disk)
+       */
       type: { type: String },
+      /**
+       * Service payload
+       */
       payload: mongoose.Schema.Types.Mixed,
     } ],
   } ],
