2389c990d3e585132e7db2423ce1cadebac4abda
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-02 21:17:55 +0300

    Database connection

--- src/workspaces.ts
+++ src/workspaces.ts
@@ -1,5 +1,25 @@
 import fs from 'fs';
 import YAML from 'yaml';
+import mongoose from 'mongoose';
+const Workspace = require('./models/workspace');
+
+/**
+ * Connecting to mongodb
+ */
+function start(): void {
+  try {
+    const url = 'mongodb+srv://xema:wCyPjqzoe7JRiGUX@cluster0.nxtjs.mongodb.net/workspaces';
+
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  getWorkspaces();
+}
 
 /**
  * Get information from workspaces configuration-file
