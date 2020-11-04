9d603ef3ebbdbadec28e57a967b555ac71b25c2e
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-04 16:20:20 +0300

    Fixed return tpye

--- src/services/workspace.ts
+++ src/services/workspace.ts
@@ -1,5 +1,5 @@
 import Workspace from '../database/models/workspace';
-import mongoose from 'mongoose';
+import IWorkspace from '../types/workspace';
 
 /**
  * Workspace service
@@ -9,7 +9,7 @@ export default class WorkspacesService {
    * @param {object} workspaceOptions - Workspace options for looking for documents
    * @returns {Promise<mongoose.Document | null} - Promise
    */
-  public static async find(workspaceOptions: object): Promise<mongoose.Document | null> {
-    return Workspace.findOne(workspaceOptions);
+  public static async find(workspaceOptions: object = {}): Promise<IWorkspace[] | null> {
+    return Workspace.find(workspaceOptions);
   }
 }
