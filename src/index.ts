1c53a169be48b3ca33ef5c02d73995aa8a9d1fa8
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-04 17:11:37 +0300

    Fix merge error

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,37 @@
 import app from './app';
-import Config from './config/index';
+import Config from './config';
 import ws from 'ws';
+import Request from './types/request';
+import WorkspacesController from './controllers/workspaces';
 
 app.listen(Config.httpPort, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${Config.httpPort}`);
 });
 
-const server = new ws.Server({ port: Config.wsPort }, () => {
-  console.log(`⚡️[server]: Server is running at ws://localhost:${Config.wsPort}`);
+const server = new ws.Server({
+  port: Config.wsPort,
+  path: '/client',
+}, () => {
+  console.log(`⚡️[server]: Server is running at ws://localhost:${Config.wsPort}/client`);
 });
 
 server.on('connection', (socket) => {
-  socket.send('It works!');
+  socket.send('Сonnected!');
 
-  socket.on('message', (data) => {
-    console.log(data);
-    socket.send('Hi!');
+  socket.on('message', async (data: string) => {
+    const dataObj: Request = JSON.parse(data.toString());
+
+    let result;
+
+    switch (dataObj.type) {
+      case 'getWorkspaces':
+        result = await WorkspacesController.getWorkspaces();
+        break;
+    }
+
+    socket.send(JSON.stringify({
+      messageId: dataObj.messageId,
+      response: result,
+    }));
   });
 });
