b26c048f6c15a908cc002fa5cb9f4f28fe97027b
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-05 21:46:16 +0300

    Add comments

--- src/types/workspaceConfig.ts
+++ src/types/workspaceConfig.ts
@@ -4,6 +4,9 @@ import Workspace from './workspace';
  * Interface for service
  */
 interface ConfigData {
+  /**
+   * Array of all workspaces from config-file
+   */
   workspaces: Workspace[];
 }
 
