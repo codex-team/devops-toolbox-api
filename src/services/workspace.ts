3bc942b19033769aebd506e51d6dbe80dd45d915
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-06 14:16:34 +0300

    Added method to update services

--- src/services/workspace.ts
+++ src/services/workspace.ts
@@ -1,16 +1,32 @@
 import mongoose from '../database';
 import Workspace from '../database/models/workspace';
 import IWorkspace from '../types/workspace';
+import Service from '../types/service';
 
 /**
  * Workspace service
  */
 export default class WorkspacesService {
   /**
+   *
    * @param workspaceOptions - Workspace options for looking for documents
    * @returns - Workspaces
    */
   public static async find(workspaceOptions: mongoose.FilterQuery<typeof Workspace> = {}): Promise<IWorkspace[] | null> {
     return Workspace.find(workspaceOptions);
+  }
+
+  /**
+   *
+   * @param token - Server token
+   * @param actualServices - Actual services
+   */
+  public static async updateServices(token: string | undefined, actualServices: Service[]): Promise<IWorkspace | null> {
+    return Workspace.findOneAndUpdate({ 'servers.token': token }, {
+      $set: {
+        'servers.$.services': actualServices,
+      },
+      new: true,
+    });
   }
 }
