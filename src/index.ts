02c4ced5c4a10e0110d832df211afa50d667e645
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-07 23:46:28 +0300

    Add user token reader

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,21 @@ const server = new ws.Server({
   console.log(`⚡️[server]: Server is running at ws://localhost:${Config.wsPort}/client`);
 });
 
-server.on('connection', (socket) => {
+server.on('connection', (socket, req) => {
   socket.send('Сonnected!');
 
   socket.on('message', async (data: string) => {
     const dataObj: Request = JSON.parse(data.toString());
 
     let result;
+    const userToken = req.headers['sec-websocket-protocol'];
 
     switch (dataObj.type) {
       case 'getWorkspaces':
         result = await WorkspacesController.getWorkspaces();
+        break;
+      case 'getWorkspace':
+        result = await WorkspacesController.getWorkspace(userToken);
         break;
     }
 
