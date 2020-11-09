e4a64fbc29ec0f7f2f579b882d1f1be41cf609d2
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-09 12:31:54 +0300

    Update agent.ts
    1. Added sending updated workspace to client
    2. Added method for adding workspace

--- src/controllers/agent.ts
+++ src/controllers/agent.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import ws from 'ws';
 import WorkspacesService from '../services/workspace';
+import { clients } from '../index';
 
 /**
  * Agent controller
@@ -14,9 +16,35 @@ export default class AgentController {
   public static async updateServices(req: express.Request, res: express.Response): Promise<void> {
     const workspace = await WorkspacesService.updateServices(req.headers.authorization, req.body.services);
 
+    if (workspace) {
+      const client: ws = clients.find(workspace.authToken);
+
+      client?.send(JSON.stringify(workspace));
+    }
+
     res.json({
       success: !!workspace,
       workspace,
+    });
+  }
+
+  /**
+   * Add workspace
+   *
+   * @param req - request
+   * @param res - response
+   */
+  public static async addWorkspace(req: express.Request, res: express.Response): Promise<void> {
+    const workspace = {
+      name: req.body.name,
+      authToken: req.body.authToken,
+      servers: req.body.servers,
+    };
+
+    const workspaceDocument = await WorkspacesService.add(workspace);
+
+    res.json({
+      workspaceDocument,
     });
   }
 }
