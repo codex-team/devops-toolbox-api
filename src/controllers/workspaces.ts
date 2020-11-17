e3b0a6c42b4f90a03090e1a9cc138797a9ba70de
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-17 19:27:23 +0300

    Added method

--- src/controllers/workspaces.ts
+++ src/controllers/workspaces.ts
@@ -1,5 +1,6 @@
 import WorkspacesService from '../services/workspace';
 import Workspace from '../types/workspace';
+import express from 'express';
 
 /**
  * Workspace controller
@@ -10,5 +11,25 @@ export default class WorkspacesController {
    */
   public static async getWorkspaces(): Promise<Workspace [] | null> {
     return WorkspacesService.find();
+  }
+
+  /**
+   * Add workspace
+   *
+   * @param req - request
+   * @param res - response
+   */
+  public static async addWorkspace(req: express.Request, res: express.Response): Promise<void> {
+    const workspace = {
+      name: req.body.name,
+      authToken: req.body.authToken,
+      servers: req.body.servers,
+    };
+
+    const workspaceDocument: Workspace | null = await WorkspacesService.add(workspace);
+
+    res.json({
+      workspaceDocument,
+    });
   }
 }
