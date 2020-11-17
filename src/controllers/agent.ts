a5c1b4ba4ea4ca957e01411fe570f464e4c3777b
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-17 21:17:41 +0300

    Fixed response

--- src/controllers/agent.ts
+++ src/controllers/agent.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import WorkspacesService from '../services/workspace';
 import Client from '../types/client';
 import ClientsList from '../utils/clientsList';
+import Workspace from '../types/workspace';
+import Response from '../types/response';
 
 /**
  * Agent controller
@@ -14,37 +16,25 @@ export default class AgentController {
    * @param res - response
    */
   public static async updateServices(req: express.Request, res: express.Response): Promise<void> {
-    const workspace = await WorkspacesService.updateServices(req.headers.authorization, req.body.services);
+    const workspace: Workspace | null = await WorkspacesService.updateServices(req.headers.authorization, req.body.services);
 
     if (workspace) {
       const clients: Client[] | undefined = ClientsList.getClients().find(workspace._id.toString());
 
-      clients?.forEach(client => client.socket.send(JSON.stringify(workspace)));
+      const wsResponse: Response = {
+        messageId: null,
+        type: 'updateServices',
+        payload: {
+          updatedWorkspace: workspace,
+        },
+      };
+
+      clients?.forEach(client => client.socket.send(JSON.stringify(wsResponse)));
     }
 
     res.json({
       success: !!workspace,
       workspace,
-    });
-  }
-
-  /**
-   * Add workspace
-   *
-   * @param req - request
-   * @param res - response
-   */
-  public static async addWorkspace(req: express.Request, res: express.Response): Promise<void> {
-    const workspace = {
-      name: req.body.name,
-      authToken: req.body.authToken,
-      servers: req.body.servers,
-    };
-
-    const workspaceDocument = await WorkspacesService.add(workspace);
-
-    res.json({
-      workspaceDocument,
     });
   }
 }
