34801ea09beaa0210b49d0b05e05db21c34fa029
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-20 21:58:33 +0300

    Updated JSDoc

--- src/utils/protocol/types/transportOptions.ts
+++ src/utils/protocol/types/transportOptions.ts
@@ -1,5 +1,14 @@
+/**
+ * Transport options for class Transport
+ */
 interface TransportOptions {
+  /**
+   * The Port you are using for web socket connection
+   */
   port: number;
+  /**
+   * Accept only connections matching this path
+   */
   path?: string;
 }
 
