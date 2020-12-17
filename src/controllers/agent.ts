07718d120d488b51d2cbbca1abcc63e08dcd6d5e
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-12-17 23:33:29 +0500

    Update agent.ts

--- src/controllers/agent.ts
+++ src/controllers/agent.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import WorkspacesService from '../services/workspace';
 import { Workspace } from '../types';
+import Client from '../utils/ctproto/client';
+import { NewMessage, ResponseMessage } from '../utils/ctproto/types';
+
+type Paylaod = Record<string, string>;
 
 /**
  * Agent controller
@@ -16,9 +20,9 @@ export default class AgentController {
     const workspace: Workspace | null = await WorkspacesService.updateServices(req.headers.authorization, req.body.services);
 
     if (workspace) {
-      req.app.context.transport
+      req.app.locals.transport
         .clients
-        .find((client) => client.authData.workspaceIds.includes(workspace._id))
+        .find((client: Client<Paylaod, ResponseMessage<Paylaod>, NewMessage<Paylaod>>) => client.authData.workspaceIds.includes(workspace._id))
         .send('workspace-updated', { workspace });
     }
 
