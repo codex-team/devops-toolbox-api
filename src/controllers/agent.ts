0c8579180caa80d970fb2ace87dc6df4cca5c0af
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-18 20:46:08 +0300

    Changed filter to find

--- src/controllers/agent.ts
+++ src/controllers/agent.ts
@@ -18,7 +18,7 @@ export default class AgentController {
     if (workspace) {
       req.app.context.transport
         .clients
-        .find((client) => client.authData.workspaces.filter(w => w._id.toString() === workspace._id.toString()).length > 0)
+        .find((client) => client.authData.workspaces.find(w => w._id.toString() === workspace._id.toString()) !== undefined)
         .send('workspace-updated', { workspace });
     }
 
