af9773a46b4480dd25a2c46545e6294adfa6c7ea
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-01-29 21:03:53 +0300

    Added services aggregation type

---
+++ src/types/servicesAggregation.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface for server
+ */
+export default interface ServicesAggregation {
+  /**
+   * ID of aggregation
+   */
+  id: null;
+
+  /**
+   * List of services
+   */
+  servicesList: any;
+}
